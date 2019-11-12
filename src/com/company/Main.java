4405021e603f234f12aa15aeb2b3b5e068229166
hawke38645 <hawke38645@gmail.com>
2019-11-12 10:56:27 -0600

    Added the MemberWriter class at implemented the WriteToTextFile function thus far. Work remains on the class as a whole. Also created the sub-menu for the interaction to write to file and left it in main (May have to move it to a seperate function later on).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,11 @@ public class Main {
             System.out.println("on insurance. File saving can be done in binary,");
             System.out.println("XML, and text.");
     }
+    public static void printGoodbyeMessage() {
+        System.out.println("****************************************************");
+        System.out.println("THANK YOU FOR USING THE INSURANCE SCORE CARD PROGRAM");
+        System.out.println("****************************************************");
+    }
     public static void showMenu() {
         System.out.println("Please choose an option from the menu below:");
         System.out.println("1.List all members");
@@ -20,7 +25,7 @@ public class Main {
         System.out.println("6.Save assessments as JSON file");
         System.out.println("7.Exit");
     }
-    public static void main(String[] args) {
+    public static <label> void main(String[] args) {
         printWelcomeMessage();
         //Collect Input for file name
         Scanner sc = new Scanner(System.in);
@@ -35,28 +40,53 @@ public class Main {
         //Now we will be showing the menu and taking in the user's choice (an integer).
         showMenu();
         int choice = sc.nextInt();
+        LOOP:
         do {
-            if(choice == 1) {
+            if (choice == 1) {
                 //List all members
                 MemberPrinter.printInsuranceMember(InsuranceMembers);
             }
-
-            else if(choice == 2) {
+            else if (choice == 2) {
                 //Add a new member
                InsuranceMembers.add(AddMember.addNewMember());
             }
-            else if(choice == 3) {
+            else if (choice == 3) {
                 //Save members(As text(T), binary(B), or XML(X))
+                Scanner nsc = new Scanner(System.in);
+                System.out.print("Please enter the name of the file to create: ");
+                String fileName = nsc.nextLine();
+                System.out.println("What kind of file would you like to create?");
+                System.out.println("1.Text File   (Enter T or t)");
+                System.out.println("2.Binary File (Enter B or b)");
+                System.out.println("3.XML File    (Enter X or x)");
+                System.out.print("Please enter your choice: ");
+                String fileChoice = nsc.nextLine();
+                    if (fileChoice.equals("T") || fileChoice.equals("t")) {
+                        MemberWriter.writeMembersToTextFile(fileName, InsuranceMembers);
+                    }
+                    else if (fileChoice.equals("B") || fileChoice.equals("b")) {
+                        MemberWriter.writeMembersToBinaryFile(fileName, InsuranceMembers);
+                    }
+                    else if (fileChoice.equals("X") || fileChoice.equals("x")) {
+                        MemberWriter.writeMembersToXMLFile(fileName, InsuranceMembers);
+                    }
+                    else {
+                        System.out.println("Could not verify file type.");
+                        System.out.println("Please select the 'Save Members' option again and enter t, b, or x.");
+                    }
             }
-            else if(choice == 4) {
+            else if (choice == 4) {
                 //Load members
             }
-            else if(choice == 5) {
+            else if (choice == 5) {
                 //Assess members
                 Assessor.printScoreCards(Assessor.AssessMembers(InsuranceMembers));
             }
-            else if(choice == 6) {
+            else if (choice == 6) {
                 //Save assessments as JSON file.
+            }
+            else if (choice == 7){
+                break LOOP;
             }
             else {
                 System.out.println("Please select a valid choice from above.");
@@ -64,5 +94,6 @@ public class Main {
             showMenu();
             choice = sc.nextInt();
         } while(choice != 7);
+        printGoodbyeMessage();
     }
 }
---
+++ src/com/company/MemberWriter.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MemberWriter {
+    public static void writeMembersToTextFile(String fileName, ArrayList<InsuranceMember> InsuranceMembersToWrite) {
+        try {
+            //Creating a new file and object to write to the file
+            File file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+            //Here we cycle through the ArrayList of InsuranceMember objects and use the get functions to push out each
+            //individual InsuranceMember's information with a tab. A new line is created at the end and it all repeats.
+            for (int i = 0; i < InsuranceMembersToWrite.size(); ++i) {
+                bw.write(InsuranceMembersToWrite.get(i).getFirstName()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getLastName()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getAge()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getHeight()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getWeight()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getBPSys()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getBPDias()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getCancer()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getDiabetes()+"\t");
+                bw.write(InsuranceMembersToWrite.get(i).getAlzheimers()+"\t");
+                bw.newLine();
+            }
+            bw.close();
+            System.out.println("The file has been written.");
+        }
+        catch (IOException ex) {
+            System.out.println("Could not write to file...");
+        }
+    }
+    public static void writeMembersToBinaryFile(String fileName, ArrayList<InsuranceMember> InsuranceMembersToWrite) {
+
+    }
+    public static void writeMembersToXMLFile(String fileName, ArrayList<InsuranceMember> InsuranceMembersToWrite) {
+
+    }
+}
